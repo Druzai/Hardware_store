404e922e93a8ec87815a04a69b6ab7a4c9867852
Druzai <j7world@yandex.ru>
2021-05-23 16:58:14 +0300

    Added junit tests

--- src/main/java/com/spring/services/UserDetailsServiceImpl.java
+++ src/main/java/com/spring/services/UserDetailsServiceImpl.java
@@ -33,7 +33,7 @@ public class UserDetailsServiceImpl implements UserDetailsService {
      */
     @Override
     @Transactional(readOnly = true)
-    public UserDetails loadUserByUsername(String username) {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsername(username);
         if (user == null) throw new UsernameNotFoundException(username);
 
